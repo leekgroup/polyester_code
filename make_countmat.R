e07f9305884f7df6b11e4e38a9ae15167ffc8401
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-11-24 11:58:25 -0500

    adds script for countmat.rda

---
+++ make_countmat.R
@@ -0,0 +1,11 @@
+# create the gene count matrix for the 10 selected genes 
+
+library(GenomicAlignments)
+library(ballgown)
+
+bf = list.files(pattern='*.bam$') # must only have the 7 bam files in dir
+fls = BamFileList(bf)
+names(fls) = ballgown:::ss(bf, '_', slot=1) 
+geneList = gffReadGR('ten_genes.gtf', splitByTranscript=TRUE, identifier='gene_name')
+gene_counts = summarizeOverlaps(features=geneList, reads=fls, ignore.strand=FALSE)
+countmat = assay(gene_counts)
